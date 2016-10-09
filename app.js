0ad8d457d36b6dc53c1815bc0b3d42483665e5a9
Aaron Goldsmith <aarongoldsmith@msn.com>
2016-10-08 18:26:03 -0700

    added some variables and pseudo code

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("yo")
+
+/* functions
+-press start to start the game
+-computer move
+-player move
+-compare sequences
+-if matched, then continue
+-if unmatched, end game with signal tone
+-increase tempo after 4th, 8th, and 12th move
+-end game if player takes more than 5 seconds to move
