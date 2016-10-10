343a605728370d8822ac90bc9700d2da43aa5557
Aaron Goldsmith <aarongoldsmith@msn.com>
2016-10-10 09:32:51 -0700

    beginning game logic fixed

--- app.js
+++ app.js
@@ -1,36 +1,21 @@
-console.log("yo")
 var simonSequence = [];
 var mySequence = [];
 var counter = 0;
 var roundCounter = 0;
-var yellow = document.getElementById('yellow');
-var green = document.getElementById('green');
-var blue = document.getElementById('blue');
-var red = document.getElementById('red');
-var colors = document.getElementsByClassName('section');
+$colors = $('.section');
 var isDown = false;
 
 function gameSequence(){
-
+console.log("hi")
   if (counter == 0){
-    function simonMove(){
+    simonMove();
 
-      var newMove = colors[Math.floor(Math.random() * 4) + 1];
-      simonSequence.push(newMove);
-      counter++;
-      roundCounter++;
-      for (var i=0; i < simonSequence.length; i++){
-        setTimout(1000);
-        lightUp(simonSequence[i]);
-      }
-      listenForMove();
-    }
 }
 
   if(counter == 1){
     function myMove(){
-        var newMove = colors[Math.floor(Math.random() * 4) + 1];
-        mySequence.push(newMove);
+        listenForMove();
+        mySequence.push(myMove);
         counter--;
         roundCounter++
       }
@@ -38,7 +23,19 @@ function gameSequence(){
 }
       compareArrays();
 
-$('#startButton').click(gameSequence());
+function simonMove(){
+
+  var newMove = $colors[Math.floor(Math.random() * 4) + 1];
+  simonSequence.push(newMove);
+  counter++;
+  roundCounter++;
+  for (var i=0; i < simonSequence.length; i++){
+    setTimeout(1000);
+    lightUp(simonSequence[i]);
+  }
+  listenForMove();
+}
+$('#startButton').click(gameSequence);
 
 function compareArrays() {
 for (var i = 0; i < simonSequence.length; i++){
@@ -52,89 +49,22 @@ for (var i = 0; i < simonSequence.length; i++){
 
 function gameOver() {}
 
-function lightUpColor(color){}
 
 
 
+function listenForMove(){}
 
-$(colors).on("mousedown", function(){
-      console.log('hi');      // When mouse goes down, set isDown to true
-    })
-            .on("mouseup", function() {
-      console.log('bye');  // When mouse goes up, set isDown to false
-    });
-
+function lightUp(color){
+  $colors.on("mousedown", function(){
+    console.log('hi');
+    $(this).css('filter', 'brightness(160%)');
 
 
+  })
 
-/*
-1. 1 listenForMove: function for each color section to: change color with new class while isDown = true, play sound,
-    push value 1-4 to array, increase round counter, call gameSequence()
+  $colors.on("mouseup", function() {
+    console.log('bye');
+    $(this).css('filter', 'brightness(100%)');
 
-
-
-
-
-1. determine lighter version of the same colors
-2. store new color in a variable
-3. event listener for each section mousedown
-4. if event listener is triggered for a section, append color temporarily
-5. log event to corresponding sequence array
-
--start button
-
--computer makes move
--push to computerSequence array
-
-counter++
-
--player makes move
--push to mySequence array
-
-counter--
-
-compare moves
-
-if moves are the same, computer moves again
-if moves are different, game over
-
-losing sound and square
-
-
-
-
-
-
-
-
-/*function checkMove(){
-
-
-
-document.getElementById('yellow').addEventListener("mouseup", function(){
-  this.style.backgroundColor = 'black'
-})
-
-
-
-
-/*
-
--start button
-
--computer makes move
--push to computerSequence array
-
-counter++
-
--player makes move
--push to mySequence array
-
-counter--
-
-compare moves
-
-if moves are the same, computer moves again
-if moves are different, game over
-
-losing sound and square
+  });
+}
